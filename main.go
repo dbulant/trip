2620986117ed5d9e947874c30f512f7bdad50f6c
David Bulant <david.bulant@moderntv.eu>
2017-11-24 11:36:26 +0100

    var declarations

--- main.go
+++ main.go
@@ -7,8 +7,28 @@ import (
 
 func main() {
 
-	var fun bool
-	fun = true
+	//Extended Backus-Naur Form (EBNF)
 
+	//longest variable declarartion
+	var fun bool
+	//variable assigment
+	fun = true
 	fmt.Printf("Is Go fun? %t \n", fun)
+
+	//shorter
+	// variable declaration + assigment
+	var fun1 bool = false
+	fmt.Printf("Is Go fun? %t \n", fun1)
+
+	//shorter
+	//type specification is not required
+	var fun2 = true
+	fmt.Printf("Is Go fun? %t \n", fun2)
+
+	//shortest
+	// := is shorthand for var fun3 = false
+	//may appear only inside functions
+	//default type for built-in types is bool, rune, int, float64, complex128 or string
+	fun3 := false
+	fmt.Printf("Is Go fun? %t \n", fun3)
 }
